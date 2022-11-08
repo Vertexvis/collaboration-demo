27e2262baabd1ebe4606c8f410d8ae54875c11bb
Brent Arndorfer <brent.arndorfer@vertexvis.com>
2022-11-08 16:49:42 -0600

    engine review scene

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -16,7 +16,7 @@ export const Config: Configuration = {
 // Vertex Valve
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "vpe5dKpDffDT8bLzhbP0S7Da2nN9-w-xgq6X",
+  streamKey: "ALYFSLntbchq2YCRO1Dx8ARMYxmN1dgq6v4Z",
 };
 
 export function head<T>(items?: T | T[]): T | undefined {
