6008a4d78a705636ded52d9d25ddd50102f89b95
Annastacia McCarty <39739917+amvertex@users.noreply.github.com>
2026-02-23 15:00:51 -0600

    Update stream key (#148)

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -16,7 +16,7 @@ export const Config: Configuration = {
 // Vertex Valve
 export const DefaultCredentials: StreamCredentials = {
   clientId: "08F675C4AACE8C0214362DB5EFD4FACAFA556D463ECA00877CB225157EF58BFA",
-  streamKey: "W8Nk7_Ch8u8AS4_rstyew2H6GIlvBnhf6dOc",
+  streamKey: "tlnqapBHDN4zsGNcVkDfe9XesQ4BBrRl8yAd",
 };
 
 export function head<T>(items?: T | T[]): T | undefined {
