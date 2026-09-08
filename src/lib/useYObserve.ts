c1de0eca3e7cb9e9a22a7ea12bf9e179fb11a802
Aaron Eischeid <aaron.eischeid@vertex3d.com>
2026-09-08 14:06:02 -0500

    type fixes

--- src/lib/useYObserve.ts
+++ src/lib/useYObserve.ts
@@ -2,11 +2,14 @@ import { useThrottle } from "@react-hook/throttle";
 import React from "react";
 import * as Y from "yjs";
 
-type ObserveFunc = (event: Y.YEvent, transaction: Y.Transaction) => void;
+type ObserveFunc<YT extends Y.AbstractType<any>> = (
+  event: Y.YEvent<YT>,
+  transaction: Y.Transaction
+) => void;
 
-export interface YObserve<DataT, YT> {
+export interface YObserve<DataT, YT extends Y.AbstractType<any>> {
   readonly data: DataT;
-  readonly event?: Y.YEvent;
+  readonly event?: Y.YEvent<YT>;
   readonly type: YT;
 }
 
@@ -18,22 +21,22 @@ export function useYObserve<YT extends Y.AbstractType<any>, DataT>(
   fps = 30
 ): YObserve<DataT, YT> {
   const [details, setDetails] = useThrottle(
-    { data: initial, event: undefined as Y.YEvent | undefined },
+    { data: initial, event: undefined as Y.YEvent<YT> | undefined },
     fps
   );
-  const [observer, setObserver] = React.useState<ObserveFunc>();
+  const [observer, setObserver] = React.useState<ObserveFunc<YT>>();
 
-  function update(e: Y.YEvent) {
+  function update(e: Y.YEvent<YT>) {
     setDetails({ data: serialize(), event: e });
   }
 
-  function listen(listenFn: ObserveFunc) {
+  function listen(listenFn: ObserveFunc<YT>) {
     yType.observe(listenFn);
     setObserver(listenFn);
   }
 
   React.useEffect(() => {
-    function unListen(type: Y.AbstractType<Y.YEvent>) {
+    function unListen(type: YT) {
       if (observer == null) return;
 
       type.unobserve(observer);
