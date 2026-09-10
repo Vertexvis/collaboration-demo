4d130d4b5903cbdf2c035c9a62015b85229a0326
Aaron Eischeid <aaron.eischeid@vertex3d.com>
2026-09-10 11:14:51 -0500

    upgrade eslint plugins

--- src/components/RightDrawer.tsx
+++ src/components/RightDrawer.tsx
@@ -4,7 +4,7 @@ import AccordionSummary from "@mui/material/AccordionSummary";
 import Drawer from "@mui/material/Drawer";
 import { drawerClasses } from "@mui/material/Drawer";
 import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
+import Typography, { TypographyProps } from "@mui/material/Typography";
 import React from "react";
 
 import { Message, Value } from "../lib/state";
@@ -21,7 +21,7 @@ interface Props {
   readonly open: boolean;
 }
 
-const Title = styled((props: any) => <Typography variant="body2" {...props} />)(
+const Title = styled((props: TypographyProps) => <Typography variant="body2" {...props} />)(
   () => ({ textTransform: "uppercase" })
 );
 
--- src/lib/useYObserve.ts
+++ src/lib/useYObserve.ts
@@ -2,19 +2,22 @@ import { useThrottle } from "@react-hook/throttle";
 import React from "react";
 import * as Y from "yjs";
 
-type ObserveFunc<YT extends Y.AbstractType<any>> = (
+// Yjs makes AbstractType invariant in its concrete observer event type.
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type YType = Y.AbstractType<any>;
+
+type ObserveFunc<YT extends YType> = (
   event: Y.YEvent<YT>,
   transaction: Y.Transaction
 ) => void;
 
-export interface YObserve<DataT, YT extends Y.AbstractType<any>> {
+export interface YObserve<DataT, YT extends YType> {
   readonly data: DataT;
   readonly event?: Y.YEvent<YT>;
   readonly type: YT;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function useYObserve<YT extends Y.AbstractType<any>, DataT>(
+export function useYObserve<YT extends YType, DataT>(
   yType: YT,
   initial: DataT,
   serialize: () => DataT,
